9451009ccdb617e1664ff03cb50b9bd3fff780e3
SrDucuarin <andresducuara117@gmail.com>
2023-04-23 10:00:02 -0500

    Fcode

---
+++ app.php
@@ -0,0 +1,64 @@
+<?php
+
+
+$converted = 0;
+$oped = 0;
+$operator = 0;
+
+function stConvert($x) {
+    return $x * 24;
+}
+
+function ndConvert($x) {
+    return $x * 60;
+}
+
+function rdConvert($x) {
+    return $x * 60;
+}
+
+function endly() {
+    return print("----Tenga excelente dia----");
+}
+
+function stMenu() {
+    print("----Bienvenido al convertidor de unidades de tiempo---" . "\n" . "Seleccione una de las opciones disponibles para comenzar :" . "\n" . "-[1] Dias a horas-" . "\n" . "-[2] Horas a minutos-" . "\n" . "-[3] Minutos a segundos-" . "\n" . "-[4] Salir del programa-" . "\n");
+    return print("--");
+}
+
+$stMenu = stMenu();
+$operator = fgets(STDIN);
+
+switch($operator) {
+    
+    case 1:
+        print("Ingrese los dias a convertir : ");
+        $Oped = fgets(STDIN);
+        print("Los dias ingresados corresponden a " . stConvert($Oped)) . " horas";
+        break;
+        
+    case 2:
+        print("Ingrese las horas a convertir : ");
+        $Oped = fgets(STDIN);
+        print("Las horas ingresados corresponden a " . ndConvert($Oped)) . " minutos";
+        break;
+        
+    case 3:
+        print("Ingrese los minutos a convertir : ");
+        $Oped = fgets(STDIN);
+        print("Los minutos ingresados corresponden a " . rdConvert($Oped)) . " segundos";
+        break;
+        
+    case 4:
+        print(endly());
+        break;
+    
+    default:
+        print("Opcion invalida" . "\n" . "Intente nuevamente" . "\n" . "\n");
+        $stMenu = stMenu();
+        $operator = 0;
+        
+}
+
+
+?>
